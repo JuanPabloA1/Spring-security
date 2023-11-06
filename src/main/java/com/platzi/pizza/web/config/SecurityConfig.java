8a4ba8f6459832a8668558900a7ac226530bcb09
juan <juanmeraagudelo@gmail.com>
2023-11-06 15:04:19 -0500

    requestMatchers

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.platzi.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,15 +11,16 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig {
 	
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-			.cors().and()
-			.authorizeHttpRequests()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.httpBasic(withDefaults());
-		return http.build();
-	}
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+////			.cors().and()
+//			.authorizeHttpRequests()
+//			.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+//			.anyRequest()
+//			.authenticated()
+//			.and()
+//			.httpBasic(withDefaults());
+//		return http.build();
+//	}
 }
