c4bd3fad7a34be63e8ff2b74b2972edb0d22e3fe
juan <juanmeraagudelo@gmail.com>
2023-11-06 16:42:39 -0500

    request Matchers with Roles done!

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -22,8 +22,10 @@ public class SecurityConfig {
         http.csrf().disable()
 			.cors().and()
 			.authorizeHttpRequests()
-			.requestMatchers(HttpMethod.GET, "/api/pizzas/**").permitAll()
-			.requestMatchers(HttpMethod.PUT).denyAll()
+			.requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
+			.requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
+			.requestMatchers("/api/orders/**").hasRole("ADMIN")
 			.anyRequest()
 			.authenticated()
 			.and()
@@ -38,7 +40,14 @@ public class SecurityConfig {
 				.password(passwordEncoder().encode("admin"))
 				.roles("ADMIN")
 				.build();
-		return new InMemoryUserDetailsManager(admin);
+
+		UserDetails customer = User.builder()
+				.username("customer")
+				.password(passwordEncoder().encode("customer"))
+				.roles("CUSTOMER")
+				.build();
+
+		return new InMemoryUserDetailsManager(admin, customer);
 	}
 
 	@Bean
