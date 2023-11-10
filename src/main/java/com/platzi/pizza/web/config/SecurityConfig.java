8ab2a05c37d94a849c86886b2a5ee14b3067909f
juan <juanmeraagudelo@gmail.com>
2023-11-09 21:34:29 -0500

    Method Security done!

--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.platzi.pizza.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 	
 	@Bean
@@ -18,6 +20,7 @@ public class SecurityConfig {
         http.csrf().disable()
 			.cors().and()
 			.authorizeHttpRequests()
+			.requestMatchers("/api/customers/**").hasAnyRole("ADMIN", "CUSTOMER")
 			.requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
 			.requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
 			.requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
